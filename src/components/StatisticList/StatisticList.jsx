a10163d29c3f0c986feda60e61d84e4bb7f5bca5
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-04-15 11:50:48 +0300

    fix

--- src/components/StatisticList/StatisticList.jsx
+++ src/components/StatisticList/StatisticList.jsx
@@ -6,11 +6,7 @@ export const StatisticList = ({ stats, title }) => {
   return (
     <section className={css.statistics}>
       {title && <h2 className={css.title}>{title}</h2>}
-      {/* {title ? (
-        <h2 className={css.title}>{title}</h2>
-      ) : (
-        <h2 className={css.title}>Upload stats</h2>
-      )} */}
+
       <ul className={css.stat__list}>
         {stats.map(statistic => {
           return (
